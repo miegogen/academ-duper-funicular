8892cb4aa9a24da27f201a9abdf96a7531351b20
miegogen <usualgold@gmail.com>
2023-02-08 04:10:07 +0700

    add third query on local

--- testDir/step.sql
+++ testDir/step.sql
@@ -1,3 +1,8 @@
 SELECT author, title, amount AS Количество FROM book WHERE price < 750ORDER BY 1, 3 DESC;
 
 SELECT title FROM book WHERE title LIKE 'Б%';
+
+SELECT title, author
+FROM book
+WHERE (title LIKE '_% %') AND (author LIKE '%С.%')
+ORDER BY title;
