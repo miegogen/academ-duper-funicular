fbaa30db2646b4310f0af74fcb943041d9577888
miegogen <usualgold@gmail.com>
2023-02-08 05:11:33 +0700

    add fifth query on local test_branch

--- testDir/step.sql
+++ testDir/step.sql
@@ -2,9 +2,8 @@ SELECT author, title, amount AS Количество FROM book WHERE price < 750
 
 SELECT title FROM book WHERE title LIKE 'Б%';
 
-SELECT title, author
-FROM book
-WHERE (title LIKE '_% %') AND (author LIKE '%С.%')
-ORDER BY title;
+SELECT title, author FROM book WHERE (title LIKE '_% %') AND (author LIKE '%С.%') ORDER BY title;
 
 SELECT author FROM book GROUP BY author;
+
+SELECT author, sum(amount), count(amount) FROM book GROUP BY author;
