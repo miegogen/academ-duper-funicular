25797a5b5378ff44bf878c53a12ec06c58f8a192
miegogen <usualgold@gmail.com>
2023-03-01 23:33:54 +0700

    Done with HW3

--- 03_javaBase/src/homework3_digits/Main.java
+++ 03_javaBase/src/homework3_digits/Main.java
@@ -7,30 +7,28 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a digit: ");
         int digit = scanner.nextInt();
-//        int sum = 0;
-//        int odd = 0;
+        int sum = 0;
+        int odd = 0;
         int max = 0;
-        int d;
 
-//        for (int i = 0; i <= digit; i++) {
-//            sum += i;
-//
-//            if (i % 2 != 0) {
-//                odd += i;
-//            }
-//        }
+        for (int i = 0; i <= digit; i++) {
+            sum += i;
 
-        while (digit > 0) {
-            d = digit % 10;
-            digit /= 10;
-            if (d > max) {
-                max = d;
+            if (i % 2 != 0) {
+                odd += i;
             }
         }
 
+        while (digit > 0) {
+            if ((digit % 10) > max) {
+                max = (digit % 10);
+            }
+            digit /= 10;
+        }
 
-
-        System.out.println(max);
-
+        System.out.println("Cумма цифр: " + sum);
+        System.out.println("Cумма нечётных чисел: " + odd);
+        System.out.println("Максимальная цифра: " + max);
     }
+
 }
